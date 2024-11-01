e70d9f3054fa6b892fe25541a0c7f7b7a94397e6
Torben Buck <dev@tbuck.de>
2024-11-01 15:04:55 +0100

    ✨ copy default

---
+++ totem/keymaps/tbuck/README.md
@@ -0,0 +1,4 @@
+# `tbuck` Keymap
+
+scripts seem to be broken, letters are missing.
+Trying to create a clean keymap from `default` keymap, which seems to be ~fine.
--- totem/keymaps/tbuck/config.h
+++ totem/keymaps/tbuck/config.h
@@ -22,7 +22,4 @@ for more options.
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define NO_ACTION_ONESHOT
-//#define NO_ACTION_TAPPING
-//#define NO_MUSIC_MODE
 
-#define COMBO_COUNT 1
